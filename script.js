5cbdba9dda1c8512a99846dd58125e86025065d4
Dima <dmitrymitkevich@icloud.com>
2020-11-14 21:39:19 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let a = +prompt("Введите первое значение", ""),
+    b = +prompt("Введите второе значение", ""),
+    c = prompt("Действие?", ""),
+    d = [];
+if (c == "+"){
+    alert(d = a + b);
+} else if (c == "-"){
+    alert(d = a - b);
+} else if (c == '/'){
+    alert(d = a/b);
+}
+return;
