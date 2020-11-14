415b72921d12a2fe5e631e85735451b5970a8ffe
Dima <dmitrymitkevich@icloud.com>
2020-11-15 02:37:54 +0300

    Vol2

--- script.js
+++ script.js
@@ -15,8 +15,9 @@ let a = prompt('Один из последних просмотренных фи
     c = prompt('Один из последних просмотренных фильмов?', ''),
     d = prompt('Один из последних просмотренных фильмов?', '');
 personalMovieDB.movies[a]=b;
-personalMovieDB.movies[c]=d;
+personalMovieDB.movies[c]=d; 
 console.log(personalMovieDB);
+alert();
 
              
              
