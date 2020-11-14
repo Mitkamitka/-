2267420ba565417aa7001288e9925e0cf70aaa0e
Dima <dmitrymitkevich@icloud.com>
2020-11-15 02:10:14 +0300

    Vol1 proj1

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
-let a = +prompt("Введите первое значение", ""),
-    b = +prompt("Введите второе значение", ""),
-    c = prompt("Действие?", ""),
-    d = [];
-if (c == "+"){
-    alert(d = a + b);
-} else if (c == "-"){
-    alert(d = a - b);
-} else if (c == '/'){
-    alert(d = a/b);
-}
+let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
+let personalMovieDB = {
+    count : numberOfFilms,
+    movies : {
+
+    },
+    actors : {
+
+    },
+    genres : [],
+    privat : false
+};
+let a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = prompt('На сколько оцените его?', ''),
+    c = prompt('Один из последних просмотренных фильмов?', ''),
+    d = prompt('Один из последних просмотренных фильмов?', '');
+personalMovieDB.movies[a]=b;
+personalMovieDB.movies[c]=d;
+console.log(personalMovieDB);
+
+             
+             
